4bda9e691399209ace13734f1c8d7dc882945852
Viktar Zhuk <viktorzhuk88@gmail.com>
2023-01-20 11:00:51 +0100

    create test and remove cli config

---
+++ test/specs/home.js
@@ -0,0 +1,13 @@
+
+
+// Practice E-Commerce Site – Automation Bro
+describe('Home page', () => {
+    beforeEach(async function() {
+        browser.maximizeWindow()
+    });
+
+    it('Navigate to the home page and assert the page title',async () => {
+        await browser.url('/');
+        await expect(browser).toHaveTitle('Practice E-Commerce Site – Automation Bro')
+    });
+});
