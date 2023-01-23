abd72ee3fb7720154776b0f6371a2931605e0161
Viktar Zhuk <viktorzhuk88@gmail.com>
2023-01-23 10:52:51 +0100

    nav test update

--- test/specs/nav.js
+++ test/specs/nav.js
@@ -5,7 +5,14 @@ describe('Navigation menu', () => {
     
     it('Get the text of multiple elements of the page and assert them',async () => {
        await browser.url('/')
-       await $('#menu-item-489').waitForClickable()
+       //await $('#menu-item-489').waitForClickable()
+       //using waitUnitl
+       await browser.waitUntil(async function () {
+            const homeText = await $('#menu-item-489').getText()
+            return homeText === "Home"; // return true or false, so waiting will be untill the return = true
+       }, {
+        timeoutMsg: 'Could not find "Home" element on the page' // adding error message in case the element won't be found 
+       })
         const navLinks = await $$("#primary-menu [id*='menu']");
         const expectedTitles = [ 
             'Home',
