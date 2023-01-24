cb38f1e99e20e7566a29d2762869962438b9024c
Viktar Zhuk <viktorzhuk88@gmail.com>
2023-01-24 11:10:16 +0100

    update blog.js with page obj

--- test/specs/blog.js
+++ test/specs/blog.js
@@ -1,19 +1,21 @@
+import BlogPage from '../pages/blog-page';
+
 describe('Blog page', () => {
     beforeEach(async function () {
         await browser.maximizeWindow();
     });
     
     it('Get the list of recent posts and assert the list length',async () => {
-        await browser.url('/');
-        await $("#primary-menu").waitForDisplayed();
-        const navMenu = await $$("#primary-menu [id*='menu']");
+        await BlogPage.open();
+        await BlogPage.navMenu.waitForDisplayed();
+        const navMenu = await BlogPage.NavComponent.linksNavMenu;
         for (const link of navMenu) {
             if (await link.getText() === 'Blog') {
                 await link.click()
             }
         }
 
-        const recentPosts = await $$('#recent-posts-3 ul li');
+        const recentPosts = await BlogPage.postList;
         for (const item of recentPosts) {
             const text = await item.getText();
             await expect(text.length).toBeGreaterThan(10);
