ae090ae7363188a5132af2dc396fd65d0241fc98
Viktar Zhuk <viktorzhuk88@gmail.com>
2023-01-24 12:48:01 +0100

    update contact js test

--- test/specs/contact.js
+++ test/specs/contact.js
@@ -7,7 +7,6 @@ describe('Contact Form', () => {
     
     it('Fill in the contact form and assert the alert message of success',async () => {
         await ContactPage.open();
-        // await browser.url('/');
         const navMenu = await ContactPage.NavComponent.linksNavMenu;
         for (const link of navMenu) {
             if (await link.getText() === 'Contact') {
@@ -17,13 +16,5 @@ describe('Contact Form', () => {
         await ContactPage.submitContactForm("Viktar", "admin@mail.com", "+500514645", "This is test message for testing");
         await ContactPage.alertMsg.waitForDisplayed({timeout: 8000});
         await expect(ContactPage.alertMsg).toHaveText('Thanks for contacting us! We will be in touch with you shortly');
-
-        // await $('#evf-277-field_ys0GeZISRs-1').setValue('Viktar');
-        // await $('#evf-277-field_LbH5NxasXM-2').setValue('admin@mail.com');
-        // await $('#evf-277-field_66FR384cge-3').setValue('+500514645');
-        // await $('#evf-277-field_yhGx3FOwr2-4').setValue('This is test message for testing');
-        // await $('#evf-submit-277').click()
-        // await $("[role='alert']").waitForDisplayed({timeout: 8000});
-        // await expect($("[role='alert']")).toHaveText('Thanks for contacting us! We will be in touch with you shortly')
     });
 });
