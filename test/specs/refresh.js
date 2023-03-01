51227dc53542a2233eb84d8d93587f376869ad7a
Viktar <viktorzhuk88@gmail.com>
2023-03-01 10:53:45 +0100

    adding tests for refresh.js

--- test/specs/refresh.js
+++ test/specs/refresh.js
@@ -1,5 +1,28 @@
 describe('Test to refresh the syntax', () => {
-    it('Any test name goes here', async () => {
-        
+    beforeEach(async function () {
+        await browser.maximizeWindow()
     });
+
+    it('Navigate to the page and assert the URL', async () => {
+        await browser.url('http://www.webdriveruniversity.com/')
+        const urlTitle = await browser.getUrl()
+        expect(urlTitle).toEqual('http://www.webdriveruniversity.com/')
+    });
+
+    it('Navigate to the page and assert the page title', async () => {
+        await browser.url('http://www.webdriveruniversity.com/');
+        await expect(browser).toHaveTitle('WebDriverUniversity.com')
+    });
+
+
+    it.only('Click Contact Us link on the page', async () => {
+        await browser.url('http://www.webdriveruniversity.com/');
+        await $('#contact-us').click()
+        await browser.switchWindow('WebDriver | Contact Us')
+        await $('[name=contactme]').toBeDisplayed()
+    });
+
+
+
+
 });
