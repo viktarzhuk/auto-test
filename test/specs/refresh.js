3539ced48bfd59070f8811f483ede5902e7ab142
Viktar <viktorzhuk88@gmail.com>
2023-03-01 11:58:25 +0100

    Adding tests for login form

--- test/specs/refresh.js
+++ test/specs/refresh.js
@@ -1,3 +1,5 @@
+import { faker } from '@faker-js/faker';
+
 describe('Test to refresh the syntax', () => {
     beforeEach(async function () {
         await browser.maximizeWindow()
@@ -15,11 +17,58 @@ describe('Test to refresh the syntax', () => {
     });
 
 
-    it.only('Click Contact Us link on the page', async () => {
+    it('Click Contact Us link on the page', async () => {
         await browser.url('http://www.webdriveruniversity.com/');
         await $('#contact-us').click()
-        await browser.switchWindow('WebDriver | Contact Us')
-        await $('[name=contactme]').toBeDisplayed()
+        await browser.switchWindow('http://www.webdriveruniversity.com/Contact-Us/')
+        await $('h2[class=section_header]').waitForExist()
+    });
+
+    it('Fill the contact form with some info and click submit button', async () => {
+        await browser.url('http://www.webdriveruniversity.com/');
+        await $('#contact-us').click();
+        await browser.switchWindow('http://www.webdriveruniversity.com/Contact-Us/');
+        await $('input[name = first_name]').setValue('Viktar');
+        await $('input[name = last_name]').setValue('Zhuk');
+        await $('input[name = email]').setValue('test@gmail.com');
+        await $('textarea[name = message]').setValue('Hello, this is test message');
+        await $('input[type=submit]').click();
+        const pageText = await $("//*[text() = 'Thank You for your Message!']").getText()
+        await expect(pageText).toEqual('Thank You for your Message!')
+    });
+
+    it('Fill the contact form with some info and click reset button', async () => {
+        await browser.url('http://www.webdriveruniversity.com/');
+        await $('#contact-us').click();
+        await browser.switchWindow('http://www.webdriveruniversity.com/Contact-Us/');
+        await $('input[name = first_name]').setValue('Viktar');
+        await $('input[name = last_name]').setValue('Zhuk');
+        await $('input[name = email]').setValue('test@gmail.com');
+        await $('textarea[name = message]').setValue('Hello, this is test message');
+        await $('input[type=reset]').click()
+        const placeholder = await $('input[name = first_name]').getAttribute('placeholder')
+        await expect(placeholder).toEqual('First Name')
+    });
+
+    it('Fill in the contact form with random data and submit', async () => {
+        await browser.url('http://www.webdriveruniversity.com/');
+        await $('#contact-us').click();
+        await browser.switchWindow('http://www.webdriveruniversity.com/Contact-Us/');
+        await $('input[name = first_name]').setValue(faker.name.firstName());
+        await $('input[name = last_name]').setValue(faker.name.lastName());
+        await $('input[name = email]').setValue(faker.internet.email());
+        await $('textarea[name = message]').setValue(faker.lorem.paragraphs(2));
+        await $('input[type=reset]').click();
+        const placeholder = await $('input[name = first_name]').getAttribute('placeholder')
+        await expect(placeholder).toEqual('First Name');
+    });
+
+    it.only('Navigate to login form page and assert the page title', async () => {
+        await browser.url('http://www.webdriveruniversity.com/');
+        await $('#login-portal').click()
+        await browser.switchWindow('http://www.webdriveruniversity.com/Login-Portal/');
+        await expect(browser).toHaveUrlContaining('Login-Portal')
+        await expect($('#text')).toBeDisplayed()
     });
 
 
