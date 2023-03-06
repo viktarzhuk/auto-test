08d3d56ef31e79ddf69bce9e4f02ee6d5a7722d3
Viktar Zhuk <viktorzhuk88@gmail.com>
2023-03-06 13:04:16 +0100

    API tests get api with users and get user by ID

--- test/specs/users.js
+++ test/specs/users.js
@@ -1,2 +1,27 @@
 import supertest from 'supertest';
-const request = supertest('https://gorest.co.in/public/v2');
+const request = supertest('https://gorest.co.in/public/v2/');
+import {expect} from 'chai';
+
+const TOKEN = 'c00377321ff01fc5c67198d192fe717f065acf09065c878e8a89896b2fda3776'
+
+describe('Users API', () => {
+    it('GET/Users',async () => {
+        await request
+       .get(`users?access-token=${TOKEN}`)
+       .then(function (res) {
+        // console.log(res.body)
+        expect(res.body).to.not.be.empty;
+       })          
+   
+    });
+
+    it.only('GET /users/<id>', async () => {
+        await request
+        .get(`users/838001?access-token=${TOKEN}`)
+        .then((res) => {
+            console.log(res.body)
+            expect(res.body).to.not.be.empty;
+        })
+    });
+        
+});
