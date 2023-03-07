ad0cbe7b2b7404d57b2eb28f9a82e3230d90f697
Viktar Zhuk <viktorzhuk88@gmail.com>
2023-03-07 11:26:54 +0100

    Negative scenarious tests

--- test/specs/post.js
+++ test/specs/post.js
@@ -41,3 +41,43 @@ describe('User Posts', () => {
         .expect(200)
     });
 });
+
+
+/////////////////////////////NEGATIVE SCENARIOUS FOR APIs/////////////////////////////////
+
+describe.only('Negative tests', () => {
+    
+    it('401 Authentication failed - request without authentication token',async () => {
+        const data = {
+            field: "User",
+            message: faker.lorem.paragraphs(1),
+            user_id: 777777,
+            title : "Argumentum ubi tolero est bis accipio voluptas temporibus decretum tempore temptatio titulus.",
+            body : "Ventosus ultio degero. Tum canonicus dens. Spoliatio credo surgo. Virtus suffoco tamquam."
+    }
+
+   const res = await request 
+    .post('posts')
+    .send(data);
+     expect(res.status).to.equal(401)
+     expect(res.text).to.equal('{"message":"Authentication failed"}')
+    console.log(res)
+    });
+
+    it.only('422 data validation failed - request without proper data in the body', async () => {
+        const data = {
+            field: "User",
+            message: faker.lorem.paragraphs(1),
+            user_id: 777777,
+            title : "Argumentum ubi tolero est bis accipio voluptas temporibus decretum tempore temptatio titulus.",
+        }
+
+   const res = await request 
+    .post('posts')
+    .set('Authorization', `Bearer ${TOKEN}`)
+    .send(data);
+     expect(res.status).to.equal(422)
+     expect(res.text).to.equal('[{"field":"body","message":"can\'t be blank"}]')
+    console.log(res)
+    });
+});
