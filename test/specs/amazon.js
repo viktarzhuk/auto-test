4ac3a90bb54cb1a5b283473081365d9b53efdb50
Viktar Zhuk <viktorzhuk88@gmail.com>
2023-03-10 12:41:22 +0100

    Amazon test suit

---
+++ test/specs/amazon.js
@@ -0,0 +1,86 @@
+describe('Test suite for amazon.com', () => {
+    beforeEach(async () => {
+        await browser.maximizeWindow();
+        await browser.url('https://www.amazon.com/');
+    });
+    
+    
+    it('Navigate to amazon.com and asser the page title', async () => {
+        await expect(browser).toHaveTitleContaining('Amazon');
+    });
+
+    it('Navigate to the page and check that nav header with main nav menu elements is displayed', async () => {
+        await $('#navbar').isDisplayed();
+        await $('#nav-logo-sprites').isDisplayed();
+        await $('#glow-ingress-block').isDisplayed();
+        await $('#nav-search').isDisplayed();
+        await $('#icp-nav-flyout').isDisplayed();
+        await $('#nav-link-accountList').isDisplayed();
+        await $('#nav-orders').isDisplayed();
+        await $('#nav-cart').isDisplayed();
+    });
+
+    it('Verify that "Choose your location" popup appears on clikcing "Deliver To" menu in the nav header menu ', async () => {
+        await $('#nav-global-location-popover-link').waitForDisplayed();
+        await $('#nav-global-location-popover-link').click();
+        await $('#a-popover-header-1').waitForDisplayed();
+        await expect($('#a-popover-header-1')).toHaveText('Choose your location');
+    });
+
+    it('Verify "Choose your location" popup elements', async () => {
+        await $('#nav-global-location-popover-link').waitForDisplayed();
+        await $('#nav-global-location-popover-link').click();
+        await $('#a-popover-content-1').waitForDisplayed();
+        await $("div#GLUXAddressBlock > .a-padding-none.a-row.a-section.a-spacing-small").waitForDisplayed();
+        const text = await $("div#GLUXAddressBlock > .a-padding-none.a-row.a-section.a-spacing-small").getText();
+        await expect(text).toEqual('Delivery options and delivery speeds may vary for different locations');
+        const buttonText = await $('span#GLUXSignInButton  .a-button-text').getText();
+        await expect(buttonText).toEqual('Sign in to see your addresses');
+        await expect($("//h5[text()='or enter a US zip code']")).toHaveText('or enter a US zip code');
+        await $('#GLUXZipUpdateInput').isDisplayed();
+        await expect($('#GLUXZipUpdate-announce')).toHaveText('Apply')
+        await expect($("//h5[text()='or']")).toHaveText('or');
+        await $('#GLUXCountryList').isDisplayed();
+        await expect($("button[name='glowDoneButton']")).toHaveText('Done')
+    });
+
+    it('Verify dropdown with the list of countries available for shipping in the "Choose your location" popup', async () => {
+        await $('#nav-global-location-popover-link').waitForDisplayed();
+        await $('#nav-global-location-popover-link').click();
+        await $('#a-popover-content-1').waitForDisplayed();
+        await $('#GLUXCountryList').waitForEnabled();
+        await $("//span[@class='a-dropdown-prompt']").click();
+        const dropdownItems = await $$("//ul[@role='listbox']/li/a");
+        const itemText = await dropdownItems.map(function(item) {
+            return item.getText()
+        })
+        await expect(itemText.length).toEqual(246)
+    });
+
+    it('Verify the value from the dropdown with list of countries can be selected', async () => {
+        await $('#nav-global-location-popover-link').waitForDisplayed();
+        await $('#nav-global-location-popover-link').click();
+        await $('#a-popover-content-1').waitForDisplayed();
+        await $('#GLUXCountryList').waitForEnabled();
+        await $("//span[@class='a-dropdown-prompt']").click();
+        await $("//ul[@role='listbox']/li/a[text()='Bahrain']").click();
+        await expect($('#GLUXCountryValue')).toHaveText('Bahrain');
+    });
+
+    it('Verify that the selected country in the "Choose your location" popup is displayed in the global header nav menu after clicking "Done" button', async () => {
+        await $('#nav-global-location-popover-link').waitForDisplayed();
+        await $('#nav-global-location-popover-link').click();
+        await $('#a-popover-content-1').waitForDisplayed();
+        await $('#GLUXCountryList').waitForEnabled();
+        await $("//span[@class='a-dropdown-prompt']").waitForDisplayed()
+        await $("//span[@class='a-dropdown-prompt']").click();
+        await $("//ul[@role='listbox']/li/a[text()='Bahrain']").click();
+        await expect($('#GLUXCountryValue')).toHaveText('Bahrain');
+        await $("button[name='glowDoneButton']").click();
+        await expect($('#glow-ingress-line2')).toHaveText('Bahrain');
+    });
+
+    it('Verify the dropdown with search categories on the global header nav menu', async () => {
+        
+    });
+});
